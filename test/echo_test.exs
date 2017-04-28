87c6458ba87ce91f8495e96896e5c8ee45c3af86
Miel Donkers <miel.donkers@codecentric.nl>
2017-04-28 16:23:58 +0200

    Echo server

---
+++ test/echo_test.exs
@@ -0,0 +1,12 @@
+defmodule OTP.EchoTest do
+  use ExUnit.Case, async: true
+  alias OTP.Echo
+
+  test "echo" do
+    {:ok, pid} = Echo.start_link()
+    Echo.send(pid, :hello)
+    assert_receive :hello
+    Echo.send(pid, :hi)
+    assert_receive :hi
+  end
+end
